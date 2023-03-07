52b14d9f670ca016e805952b2d127111a5e952bb
Luís Ribeiro <a100608@alunos.uminho.pt>
2023-03-07 17:40:27 +0000

    Aula 2 de SO

---
+++ Aula_2/pid.c
@@ -0,0 +1,81 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    /* pid_t pid = getpid();  // pid do processo atual
+    pid_t ppid = getppid();  // pid do pai do processo atual
+    printf("pid : %d; ppid: %d\n", pid, ppid); */
+
+    if(argc == 1)
+    {
+        pid_t pid, terminated;
+        int i, status;
+        for(i = 0; i < 10; i++)
+        {
+            if((pid = fork()) == 0)
+            {
+                //filho 
+                printf("[filho %d] pid = %d\n", i + 1, getpid());
+                printf("[filho %d] ppid = %d\n", i + 1, getppid());
+                _exit(i+1);
+            }
+            else
+            {
+                //pai
+                terminated = wait(&status);
+                printf("[pai] Código de saída: %d\n", WEXITSTATUS(status));
+            }
+
+        }
+    }
+    else
+    {
+        pid_t pid, terminated;
+        pid_t child1, child2, child3, child4, child5, child6, child7, child8, child9, child10; 
+        int i, status;
+        for(i = 0; i < 10; i++)
+        {
+            if(!(pid = fork()))
+            {
+                //filho
+                printf("[filho %d] pid = %d\n", i + 1, getpid());
+                printf("[filho %d] ppid = %d\n", i + 1, getppid());
+                _exit(i+1);
+            }
+        }
+        for(i = 0; i < 10; i++)
+        {
+            terminated = wait(&status);
+            printf("[pai] Código de saída: %d do processo %d\n", WEXITSTATUS(status), terminated);
+        }
+    }
+
+
+
+    /* if((pid = fork()) == 0)
+    {
+        //filho
+        printf("[filho %d] pid = %d\n", i, getpid());
+        printf("[filho] ppid = %d\n", getppid());
+        _exit(i);
+    }
+    else
+    {
+        //pai
+        printf("[pai] pid = %d\n", getpid());
+        printf("[pai] ppid = %d\n", getppid());
+        printf("[pai] pid filho = %d\n", pid);
+        i--;
+        printf("[pai] i = %d\n", i);
+
+        int status;
+        pid_t terminated = wait (&status);
+        if (WIFEXITED(status))
+            printf("[pai] filho %d terminated\n", terminated);
+        else
+            printf("error");
+    } */
+    return 0;
+}
